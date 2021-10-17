c59746e5ebfffd2785a43e6ec3bc9e43150da54e
Wendy Nguyen <wendy.nguyen@gmail.com>
2021-10-16 21:10:55 -0700

    added query by support

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const app = express();
 const { animals } = require('./animals');
 console.log(animals);
 
+function findById(id, animalsArray) {
+  const result = animalsArray.filter(animal => animal.id === id)[0];
+  return result;
+}
+
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
     // Note that we save the animalsArray as filteredResults here:
@@ -48,6 +53,15 @@ app.get('/api/animals', (req, res) => {
     results = filterByQuery(req.query, results);
   }
   res.json(results);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+  const result = findById(req.params.id, animals);
+  if(result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
 });
 
 app.listen(3001, () => {
